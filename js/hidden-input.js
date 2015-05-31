f9f77412c4dd24db4d0001d79e0e28f8f103900f
Colin Zheng <zheng_colin@yahoo.com>
2015-05-31 14:34:51 -0400

    fixed radio imitation bug for button group on contact page

--- js/hidden-input.js
+++ js/hidden-input.js
@@ -1,7 +1,16 @@
   var btns = ['btn-1','btn-2','btn-3','btn-4'];
   var input = document.getElementById('btn-input');
   for(var i = 0; i < btns.length; i++) {
+
+    // on click, set set hidden input value and set background color
     document.getElementById(btns[i]).addEventListener('click', function() {
       input.value = this.value;
+
+		// reset all inline styles
+		for(var j = 0; j<btns.length; j++) {
+			$('#' + btns[j]).attr("style"," ");
+		};
+
+      $('#' + this.id).attr("style","background-color:#CCC");
     });
   }
